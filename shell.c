e79fafc336cefce319f0c242ee9e6693651af75f
tanmay5268 <tanmay.mewati94@gmail.com>
2025-01-17 14:47:03 +0530

    added execve to execute commands

--- shell.c
+++ shell.c
@@ -1,4 +1,9 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void){
     /* declare variables */
@@ -13,18 +18,36 @@ int main(void){
     characters_read= getline(&line, &len, stdin);/* stored in a variable cuz a fx always returns a value  */
     if (characters_read==-1){
         perror("error reading input");
-        return (-1);
+        return (-1); 
     }
     else{
         /* process the input using tokenization */
         char *token = strtok(line, " ");
         while (token != NULL)
         {
-            printf("%s\n",token);
-            token = strtok(NULL, " ");
+            /* this is where to fork */
+            pid_t pid = fork();
+            if (pid == -1){
+                perror("fork failed");
+                return (-1);
             }
+            else if (pid == 0){
+                /* child process */
+                char *cmdPath = path_check(token);/* to find comand path */
+                if (cmdPath == NULL){
+                    write(2, "command not found\n", 18);
+                    exit(1);/* exit with an error */
+                }
+                /* execution of program with execve */
+                execve(cmdPath, &token, __environ);
+                /* check if execve fails */
+                if(execve(cmdPath, &token, __environ) == -1){
+                perror("failed to execute the program");
+                return EXIT_FAILURE;
+                }
+            }
+        }
     }
-    
-    
-   
+
+
 }
