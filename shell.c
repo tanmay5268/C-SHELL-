f7fd2a0db2591dfadb4d424d5a93457c918f078e
tanmay5268 <tanmay.mewati94@gmail.com>
2025-01-17 17:12:24 +0530

    files were not found

--- shell.c
+++ shell.c
@@ -11,6 +11,7 @@ char *path_check(char *command) /* this command takes the tokenize string from {
     {
         char *full_path = malloc(strlen(dir) + strlen(command) + 2);
         sprintf(full_path, "%s/%s", dir, command);
+        printf("full path is %s",full_path);
         if (access(full_path, X_OK) == 0){
             free(path_copy);
             return (full_path);
